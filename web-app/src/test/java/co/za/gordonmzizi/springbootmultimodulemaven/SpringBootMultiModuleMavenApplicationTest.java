d068c1ae3c798e9222ad506474ad55c7814f91eb
SbonisoGordonMzizi <gordonmzizi@gmail.com>
2021-07-06 20:40:23 +0200

    base code

---
+++ web-app/src/test/java/co/za/gordonmzizi/springbootmultimodulemaven/SpringBootMultiModuleMavenApplicationTest.java
@@ -0,0 +1,10 @@
+package co.za.gordonmzizi.springbootmultimodulemaven;
+
+import org.junit.jupiter.api.Test;
+
+class SpringBootMultiModuleMavenApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
